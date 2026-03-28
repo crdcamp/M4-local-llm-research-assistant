a87d7de3fc0da580fcb6f7f491f7c32b1c725fca
Christian Camp <crdcamp@gmail.com>
2026-03-27 19:27:29 -0600

    Refactoring for testing

--- testing/generate_searches.py
+++ testing/generate_searches.py
@@ -2,31 +2,39 @@
 from llama_cpp import Llama
 import os
 
-# %% Define file paths
-models_dir = "models"
+# %% Define file paths and model parameters
+models_dir = "../models"
 llm_qwen = "Qwen3-8B-Q8_0.gguf"
-llm_glm_9B_8bit = "GLM-4.6V-Flash-GGUF"
+llm_glm_9B_8bit = "GLM-4.6V-Flash-Q8_0.gguf"
 os.makedirs(models_dir, exist_ok=True)
 
-# %% Load model
-# Link to model: https://huggingface.co/Qwen/Qwen3-8B-GGUF/blob/main/Qwen3-8B-Q8_0.gguf
-model = Llama(
-    model_path = f"{models_dir}/{llm_qwen}",
+# %% Define parameters and load models
+# Links to models can be found in README
+max_tokens = 4096
+test_prompt = "Provide me 5 short internet searches to help find out the difference between endothermic and exothermic processes /no_think"
+
+model_qwen = Llama(
+    model_path=f"{models_dir}/{llm_qwen}",
+    max_tokens=max_tokens
+)
+model_glm = Llama(
+    model_path=f"{models_dir}/{llm_glm_9B_8bit}",
+    max_tokens=max_tokens
 )
 
-# %% Testing to see if the model works
-print(model.create_chat_completion(
-     messages=[{
-         "role": "user",
-         "content": "What is the meaning of life?"
-     }]
-))
 
-# %% Test initial response for requesting multiple search options
-# Add more context window. Default isn't doing it
-print(model.create_chat_completion(
+# %% Qwen Test
+print(model_qwen.create_chat_completion(
     messages=[{
         "role": "user",
-        "content": "Provide me 5 short internet searches to help find out the difference between endothermic and exothermic processes"
+        "content": test_prompt
     }]
+))
+
+# %% GLM Test
+print(model_glm.create_chat_completion(
+     messages=[{
+         "role": "user",
+         "content": test_prompt
+     }]
 ))
