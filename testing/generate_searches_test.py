02726d63808adb9297d8bfc404203e59f75004f1
Christian Camp <crdcamp@gmail.com>
2026-04-03 15:08:14 -0600

    Testing system prompts

--- testing/generate_searches_test.py
+++ testing/generate_searches_test.py
@@ -14,27 +14,45 @@ qwen_7B_I_Q4 = "Qwen2.5-7B-Instruct-Q4_K_M.gguf"
 
 os.makedirs(models_dir, exist_ok=True)
 
-# %% Load Models
-test_prompt = "Provide me 5 internet searches to to help me find out the difference between endothermic and exothermic processes"
+# %% Define Parameters, and Load Models
 
+# Parameters
+system_prompt = "You are a search query generator. When given a question or topic, output exactly five concise search engine queries a person could enter into a browser to research it. Format your response as a numbered list (1-5) with one query per line. Output only the numbered list — no preamble, explanation, or commentary."
+
+user_prompt_no_sys = "Provide me 5 internet searches to to help me find out the difference between endothermic and exothermic processes"
+user_prompt_sys = "What is the difference between endothermic and exothermic processes?"
+
+param_max_tokens = 1024
+param_verbose = True
+param_chat_format = "llama-2"
+
+# Load models
 chat_mistral_7b_Q4 = Llama(
-    model_path=f"{models_dir}/{llm_mistral_7b_4Q}",
-    max_tokens=1024,
-    chat_format="llama-2"
+    model_path=f"{models_dir}/{mistral_7b_Q4}",
+    max_tokens=param_max_tokens,
+    verbose=param_verbose,
+    chat_format=param_chat_format
 )
 
 chat_qwen_7b_Q4 = Llama(
     model_path=f"{models_dir}/{qwen_7B_I_Q4}",
-    max_tokens=1024,
-    chat_format="llama-2"
+    max_tokens=param_max_tokens,
+    verbose=param_verbose,
+    chat_format=param_chat_format
 )
 
+
+"""
+SIMPLE TESTS:
+    Just seeing if things are running smoothly. We'll add system prompts and chat
+    templates later.
+"""
 
 # %% Generate searches for Mistral
 print(chat_mistral_7b_Q4.create_chat_completion(
     messages=[{
         "role": "user",
-        "content": test_prompt
+        "content": user_prompt_no_sys
     }]
 ))
 
@@ -42,6 +60,39 @@ print(chat_mistral_7b_Q4.create_chat_completion(
 print(chat_qwen_7b_Q4.create_chat_completion(
     messages=[{
         "role": "user",
-        "content": test_prompt
+        "content": user_prompt_no_sys
     }]
+))
+
+"""
+ADDING SYSTEM PROMPTS:
+    Now let's add system prompts to see if this improves the results at all
+"""
+
+# %% Generate searches with Mistral with system prompt
+print(chat_mistral_7b_Q4.create_chat_completion(
+    messages=[
+        {
+        "role": "system",
+        "content": system_prompt
+    },
+    {
+        "role": "user",
+        "content": user_prompt_sys
+    }
+    ]
+))
+
+# %% Generate searches with Qwen with system prompt
+print(chat_qwen_7b_Q4.create_chat_completion(
+    messages=[
+        {
+        "role": "system",
+        "content": system_prompt
+    },
+    {
+        "role": "user",
+        "content": user_prompt_sys
+    }
+    ]
 ))
