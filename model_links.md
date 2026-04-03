4d6a36e7ea6705acc8b7950a7cdf94192dcd94d0
Christian Camp <crdcamp@gmail.com>
2026-04-03 13:35:19 -0600

    Found working model for search generation

---
+++ model_links.md
@@ -0,0 +1,1 @@
+https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF?show_file_info=mistral-7b-instruct-v0.1.Q4_K_M.gguf
--- testing/generate_searches.py
+++ testing/generate_searches.py
@@ -4,37 +4,22 @@ import os
 
 # %% Define file paths and model parameters
 models_dir = "../models"
-llm_qwen = "Qwen3-8B-Q8_0.gguf"
-llm_glm_9B_8bit = "GLM-4.6V-Flash-Q8_0.gguf"
+llm_mistral_7b_4Q = "mistral-7b-instruct-v0.1.Q4_K_M.gguf"
 os.makedirs(models_dir, exist_ok=True)
 
-# %% Define parameters and load models
-# Links to models can be found in README
-max_tokens = 4096
-test_prompt = "Provide me 5 short internet searches to help find out the difference between endothermic and exothermic processes /no_think"
+# %% Load Model
+test_prompt = "Provide me 5 internet searches to to help me find out the difference between endothermic and exothermic processes"
 
-model_qwen = Llama(
-    model_path=f"{models_dir}/{llm_qwen}",
-    max_tokens=max_tokens
-)
-model_glm = Llama(
-    model_path=f"{models_dir}/{llm_glm_9B_8bit}",
-    max_tokens=max_tokens
+model_mistral_7b = Llama(
+    model_path=f"{models_dir}/{llm_mistral_7b_4Q}",
+    max_tokens=1024,
+    chat_format="llama-2"
 )
 
-
-# %% Qwen Test
-print(model_qwen.create_chat_completion(
+# %% Generate searches
+print(model_mistral_7b.create_chat_completion(
     messages=[{
         "role": "user",
         "content": test_prompt
     }]
-))
-
-# %% GLM Test
-print(model_glm.create_chat_completion(
-     messages=[{
-         "role": "user",
-         "content": test_prompt
-     }]
 ))
