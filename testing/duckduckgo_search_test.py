8d32ec22b4dcdc18f1bcb4f610b23276dc5c4c43
Christian Camp <crdcamp@gmail.com>
2026-04-04 10:05:17 -0600

    Test function for web search complete

--- testing/duckduckgo_search_test.py
+++ testing/duckduckgo_search_test.py
@@ -4,8 +4,23 @@ from ddgs import DDGS
 # %% Test
 results = DDGS().text("python programming", max_results=4)
 
+href_list = []
 for r in results:
-    print(r["href"])
+    href_list.append(r["href"])
+print(href_list)
 
 # %% List comprehension method
-hrefs = [r["href"] for r in DDGS().text("python programming", max_results=3)]
+#hrefs = [r["href"] for r in DDGS().text("python programming", max_results=3)]
+
+
+# %% Testing Implementation
+from pprint import pprint
+def get_search_query_links(search_queries: list[str]) -> dict:
+    results = {}
+    for query in search_queries:
+        results[query] = [r["href"] for r in DDGS().text(query, max_results=4)]
+    pprint(results)
+    return results
+
+test_queries = ["limits of nuclear energy", "risks associated with nuclear energy", "environmental impact of nuclear energy", "safety concerns in nuclear power plants", "disadvantages of nuclear energy production"]
+get_search_query_links(test_queries)
