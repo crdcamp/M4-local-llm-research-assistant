1fc18e5d2dceb2a2e36e710ac4822e9436e274ff
Christian Camp <crdcamp@gmail.com>
2026-04-05 21:37:33 -0600

    Web page interpretation test successful (heck yea)

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ mcp = FastMCP("Research Assistant")
 print("Loading model...")
 model = Llama(
     model_path="models/Qwen2.5-7B-Instruct-Q4_K_M.gguf",
+    n_ctx = 2048,
     max_tokens=2048,
     verbose=False,
     chat_format="chatml"
