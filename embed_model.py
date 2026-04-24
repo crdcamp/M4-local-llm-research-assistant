3a497eed21da0ae74e3e98a46b48d54f1a918730
Christian Camp <crdcamp@gmail.com>
2026-04-23 20:41:51 -0600

    Foundations for embedding

--- embed_model.py
+++ embed_model.py
@@ -22,10 +22,18 @@ embed_model = Llama(
     n_ctx = 40960
 )
 
+# %% Read markdown file function
 def read_md(md_file):
     with open(md_file, 'r', encoding='utf-8') as f:
         md_content = f
     return md_content
 
-#embedding = llm.create_embedding()
-#vector = embedding["data"][0]["embedding"]
+
+# DO NOT RUN THIS YET!!!
+# Embed model with
+def embed_model():
+    for file in os.listdir(summary_dir)
+        with open(md_file, 'r', encoding='utf-8') as f:
+            md_content = f
+            embedding = llm.create_embedding(md_content)
+            #vector = embedding["data"][0]["embedding"]
