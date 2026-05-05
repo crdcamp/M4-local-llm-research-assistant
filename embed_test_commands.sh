3b1d5f4cbf9405c6197e5d4e2b126f6e98e7fd7d
Christian Camp <crdcamp@gmail.com>
2026-05-04 21:35:11 -0600

    Cleanup

--- embed_test_commands.sh
+++ embed_test_commands.sh
@@ -2,18 +2,16 @@
 
 TARGET_MODEL="Qwen3-Embedding-8B-Q6_K.gguf"
 
-SUMMARY_DIR="data/summary/"
 MODELS_DIR="/models/"
-EMBED_DIR="models/embed_models/"
+EMBED_MODEL_DIR="models/embed_models/"
+EMBED_DOCS=$(ls "data/summary"/*)
 
-echo "SUMMARY DIR: $SUMMARY_DIR"
-echo "MODELS DIR: $MODELS_DIR"
-echo -e "EMBED MODELS DIR: $EMBED_DIR\n"
+echo -e "DOCUMENTS TO EMBED:\n$EMBED_DOCS\n"
 
-for model in "$EMBED_DIR"*; do
+for model in "$EMBED_MODEL_DIR"*; do
     if [[ "$model" == *"$TARGET_MODEL" ]]; then
-        TARGET_MODEL_DIR="$EMBED_DIR$TARGET_MODEL"
-        echo -e "MODEL FOUND: $TARGET_MODEL_DIR\n"
+        TARGET_MODEL_DIR="$EMBED_MODEL_DIR$TARGET_MODEL"
+        echo -e "TARGET MODEL FOUND: $TARGET_MODEL_DIR\nEmbedding...\n"
     else
         echo "MODEL NOT FOUND\n"
     fi
