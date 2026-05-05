b1f2358f136f1bef7e90d875ef5929c4b284ecd0
Christian Camp <crdcamp@gmail.com>
2026-05-04 21:07:17 -0600

    Initial file handling logic done

--- embed_test_commands.sh
+++ embed_test_commands.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TARGET_MODEL="Qwen3-Embedding-8B-Q6_K.gguf"
+
 SUMMARY_DIR="data/summary/"
 MODELS_DIR="/models/"
 EMBED_DIR="models/embed_models/"
@@ -9,9 +11,10 @@ echo "MODELS DIR: $MODELS_DIR"
 echo -e "EMBED MODELS DIR: $EMBED_DIR\n"
 
 for model in "$EMBED_DIR"*; do
-    if [[ "$model" == *"Qwen3-Embedding-8B-Q6_K.gguf" ]]; then
-        echo "MODEL FOUND: $EMBED_DIR/$model"
+    if [[ "$model" == *"$TARGET_MODEL" ]]; then
+        TARGET_MODEL_DIR="$EMBED_DIR$TARGET_MODEL"
+        echo -e "MODEL FOUND: $TARGET_MODEL_DIR\n"
     else
-        echo "Model not found"
+        echo "MODEL NOT FOUND\n"
     fi
 done
