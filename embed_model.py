e0a6c015552c75102004c0ae8bdfad1fc2d06168
Christian Camp <crdcamp@gmail.com>
2026-05-04 20:04:34 -0600

    Changing to llama-server method

--- embed_model.py
+++ embed_model.py
@@ -1,9 +1,16 @@
+"""
+THIS CODE WILL NOT WORK FOR NEWER MODELS.
+YOU NEED TO USE LLAMA-SERVER INSTEAD (I know, you put a good amount of effort into this...)
+"""
+
 # %% Imports
 from itertools import islice
 import os
 import sys
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from llama_cpp import Llama
+
+# For later use
 import chromadb
 import time
 
@@ -51,46 +58,58 @@ documents = text_splitter.create_documents([test_md_content])
 # Show some properties of what we've just created
 print(len(documents))
 print(type(documents))
-print(documents[5])
+print(documents[1])
 
-# %% Test embedding creation
-embed_model.create_embedding(
-    # The following can take in a single document, or an array of documents.
-    # Since we're only doing a single document, we're giving it an array of one
-    [item.page_content for item in documents][:1]
-)
+
+# %% Single input embedding test
+#single_embed = embed_model.embed (documents[:1])
 
 # %% Small test embedding
 # So we're getting the same error here... hmmmmmm
 # .embed seems to work with only one field at a time...
-# So, we might just have to do it that way
-embeddings = embed_model.embed (["Hello", "World"])
+
+#embeddings = embed_model.embed (["Hello", "World"])
+
+"""
+For some reason, `create_embeddings()` is only accepting one entry, even though
+it should be able to accept multiple.
+I've found someone with a similar issue on GitHub: https://github.com/abetlen/llama-cpp-python/issues/2051
+
+I think this might be an issue with using a newer model, but before we try that let's try the following
+"""
+
+# Revised approach
+# We'll just loop through the documents and embed them one at a time instead
+# documents_embeddings = []
+# for doc in documents:
+#     emb_result = embed_model.embed(doc.page_content)
+
+"""
+Welp looks like that isn't working either!
+
+I'll tell you what: we're gonna try to embed Qwen3 8B ourselves and if
+that still doesn't work.... we'll have to try a new model
+"""
 
 # %% Final test embedding
-batch_size = 200
-documents_embeddings = []
-batches = list(chunk(documents, batch_size))
+# batch_size = 200
+# documents_embeddings = []
+# batches = list(chunk(documents, batch_size))
 
-start = time.time()
-for batch in batches:
-    embeddings = embed_model.create_embedding([item.page_content for item in batch])
-    documents_embeddings.extend(
-        [
-            (document, embeddings['embedding'])
-            for document, embeddings in zip(batch, embeddings['data'])
-        ]
-    )
-end = time.time()
+# start = time.time()
+# for batch in batches:
+#     embeddings = embed_model.create_embedding([item.page_content for item in batch])
+#     documents_embeddings.extend(
+#         [
+#             (document, embeddings['embedding'])
+#             for document, embeddings in zip(batch, embeddings['data'])
+#         ]
+#     )
+# end = time.time()
 
-all_text = [item.page_content for item in documents]
-char_per_sec = len(''.join(all_text) / (end-start))
-print(f"Time: {end-start:.2f} seconds / {char_per_sec:,.2f} chars/second")
-
-
-
-
-
-
+# all_text = [item.page_content for item in documents]
+# char_per_sec = len(''.join(all_text) / (end-start))
+# print(f"Time: {end-start:.2f} seconds / {char_per_sec:,.2f} chars/second")
 
 
 # %% Embed
