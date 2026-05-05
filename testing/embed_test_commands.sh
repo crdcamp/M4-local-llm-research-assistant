0425d7452947c76bfcb22b9ccac988a314f2ccf5
Christian Camp <crdcamp@gmail.com>
2026-05-04 20:38:13 -0600

    Directories defined

--- testing/embed_test_commands.sh
+++ testing/embed_test_commands.sh
@@ -2,6 +2,8 @@
 
 SUMMARY_DIR="$HOME/data/summary/"
 MODELS_DIR="$HOME/models/"
+EMBED_DIR="$HOME/models/embed_models"
 
-echo "$SUMMARY_DIR"
-echo "$MODELS_DIR"
+echo "Summary directory: $SUMMARY_DIR"
+echo "Models directory: $MODELS_DIR"
+echo -e "Embed models directory: $EMBED_DIR\n"
